73534ddb76a2abde4d59bad55bdff3b7214ee04d
JIAraoz <ignacioaraoz18@gmai.com>
2024-12-14 12:51:07 -0300

    about-me

--- portfolio/src/components/Home/Home.tsx
+++ portfolio/src/components/Home/Home.tsx
@@ -1,23 +1,26 @@
 
 export default function Home(){
     return(
-    <div className="home">
-        <div className="grid-left">
+    <div className="home p-24">
+        <div className="grid grid-cols-2 justify-around">
 
-        <div className="hi-there-home text-2xl text-white flex ">
-        Hi there 👋
+        <div className="grid-left justify-items-center">
+
+        <div className="hi-there-home text-2xl text-white flex mb-3 ">
+        I'm<span className="text-purple-700 pl-2"> Ignacio</span>
         </div>
         
         <p className="text-white">
-        I'm passionate about the world of web development, particularly in the PERN stack (PostgreSQL, Express, React, Node.js). I'm always eager to learn new technologies and take on new challenges.
+         A passionate about the world of web development, particularly in the PERN stack (PostgreSQL, Express, React, Node.js). I'm always eager to learn new technologies and take on new challenges.
         </p>
         
         </div>
         
-        <div className="grid-right">
-        <img src="./images/profile.png" alt='profile' className="h-96 border-black " />
+        <div className="grid-right justify-items-center">
+        <img src="./images/profile.png" alt='profile' className="h-48 rounded-full shadow-[0_0px_10px_5px_rgba(127,17,224,0.3)] animate-breathing" />
         </div>
-
+       
+        </div>
 
     </div>
     )
--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -5,7 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        'purpleX':'#27006F',
+      },
+      keyframes: {
+        breathing: {
+          '0%, 100%': { transform: 'translateY(0)' },
+          '50%': { transform: 'translateY(-3px)' },
+        },
+      },
+      animation: {
+        breathing: 'breathing 2s ease-in-out infinite',
+      }
+    },
   },
   plugins: [],
 }
