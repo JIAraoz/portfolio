9360e539b4e145d150177dfb4a86b02732cc8c56
JIAraoz <ignacioaraoz18@gmai.com>
2024-12-14 21:52:30 -0300

    feat-techSkills

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Home from './components/Home/Home'
+import Skills from './components/Skills/Skills'
 function App() {
 
 
@@ -7,6 +8,7 @@ function App() {
 
      <div className='h-screen w-screen bg-gradient-to-b from-black via-black/90 to-[#27006F]'>
         <Home></Home>
+        <Skills></Skills>
      </div>
 
   )
---
+++ portfolio/src/components/Skills/Skills.tsx
@@ -0,0 +1,17 @@
+export default function Skills(){
+    return(<>
+        <div className="pt-20 justify-center justify-items-center"><h2 className="text-gray-300 text-4xl"> TECH SKILLS</h2></div>
+        <div className="justify-items-center pt-10 grid grid-cols-3 gap-10 px-96">
+            <img src="./images/skillsIcons/atom.png " alt="React" className="h-16" />
+            <img src="./images/skillsIcons/js.png" alt="JS"  className="h-16"/>
+            <img src="./images/skillsIcons/node.png" alt="Node" className="h-16"/>
+            <img src="./images/skillsIcons/expressJS.png" alt="express" className="h-16"/>
+            <img src="./images/skillsIcons/html-5.png" alt="html5" className="h-16"/>
+            <img src="./images/skillsIcons/css-3.png" alt="css3" className="h-16"/>  
+            <img src="./images/skillsIcons/postgre.png" alt="PostgrestSQL" className="h-16" />
+            <img src="./images/skillsIcons/Sequelize.png" alt="Sequelize" className="h-16" />
+            <img src="./images/skillsIcons/typescript.png" alt="typescript" className="h-16"  />
+        </div>
+    </>
+    )
+}
