9e1412560426351929a6f83c0197f39b5e8fd899
JIAraoz <ignacioaraoz18@gmai.com>
2024-12-20 01:01:27 -0300

    feat-animation

--- portfolio/src/components/ProjectCard/ProjectCard.tsx
+++ portfolio/src/components/ProjectCard/ProjectCard.tsx
@@ -1,12 +1,17 @@
+import useIntersectionObserver from "../../hooks/useIntersectionObserver";
 import { ProjectCardProps } from "../../types/ProjectCard";
 
+
+
 export default function ProjectCard({name ,technologies, image, url}: ProjectCardProps){
-    console.log(image);
+  const {elementoRef,isIntersecting} = useIntersectionObserver({threshold:0.1})
     
     return(
-        <div className="bg-black">
+    <div  ref={elementoRef} className={`shadow-[0_0px_10px_9px_rgba(0,0,0,0.3)] hover:-translate-y-4 duration-500 h-60 ${isIntersecting ? "opacity-100 translate-y-0" : "opacity-0 translate-y-10"}`} >
+            <a href={url} target="_blank">
+
             <img src={`/images/projects/${image}.png`} alt={image} className="h-32 w-60" />
-            <div className="flex flex-col gap-2 p-3 ">
+            <div className="flex flex-col gap-4 p-3 ">
 
             <p className="text-gray-300">{name}</p>
             <div className="flex gap-2">
@@ -16,9 +21,10 @@ export default function ProjectCard({name ,technologies, image, url}: ProjectCar
             ))}
 
             </div>
-            <button><a href={url} target="_blank" >Go to Github</a></button> 
+           
             </div>
+            </a>
             
         </div>
     )
-}
+} 
