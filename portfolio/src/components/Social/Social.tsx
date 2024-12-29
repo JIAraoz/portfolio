72f204b765c6d07eb2d8452e2c08ea338f9285f3
JIAraoz <ignacioaraoz18@gmai.com>
2024-12-29 07:28:24 -0300

    resumeLoading

--- portfolio/src/components/Social/Social.tsx
+++ portfolio/src/components/Social/Social.tsx
@@ -1,18 +1,28 @@
+import { useState } from "react"
+
 export default function Social(){
     const url ="./images/social/"
+    const [isDonwloading,setDonwload] =useState(false)
+
+    const handleDonwload = ()=>{
+        setDonwload(true) 
+        setTimeout(()=>{
+            setDonwload(false)
+        },1000)
+    }
     return(
         <div className="grid grid-cols-4 w-36">
             <a href="https://www.linkedin.com/in/ignacio-araoz-23710b28b/"  target="_blank" >
-            <img src={url + "linkedin.png"} alt="linkedin" className="hover:-translate-y-1 transition-transform duration-500 h-6" />
+            <img src={url + "linkedin.png"} alt="linkedin" className="hover:-translate-y-1 hover:shadow-lg hover:shadow-white transition-all duration-500 h-6 rounded-full overflow-visible" />
             </a>
             <a href="https://github.com/JIAraoz" target="_blank">
-            <img src={url + "github.png"} alt="github" className="hover:-translate-y-1 transition-transform duration-500 h-6" />
+            <img src={url + "github.png"} alt="github" className="hover:-translate-y-1 hover:shadow-lg hover:shadow-white transition-all duration-500 h-6 rounded-full overflow-visible" />
             </a>
             <a href="mailto:ignacioaraoz18@gmail.com" > 
-            <img src={url + "gmail.png"} alt="gmail" className="hover:-translate-y-1 transition-transform duration-500 h-6"  />
+            <img src={url + "gmail.png"} alt="gmail" className="hover:-translate-y-1 hover:shadow-lg hover:shadow-white transition-all duration-500 h-6 rounded-full overflow-visible"  />
             </a>
-            <a href="./cv.pdf" download="cv.pdf">
-                <img src={url + "cv.png"} alt="donwload resume"  className="hover:-translate-y-1 transition-transform duration-500 h-6"/>
+            <a href="./cv.pdf" download="cv.pdf" onClick={handleDonwload}>
+                <img src={url + "cv.png"} alt="donwload resume"  className={`hover:-translate-y-1 hover:shadow-lg hover:shadow-white transition-all duration-500 h-6 rounded-full overflow-visible ${isDonwloading ? "cursor-progress border border-white ": ""}`}/>
             </a>
         </div>
     )
